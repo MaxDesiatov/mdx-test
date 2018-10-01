4b024a0246cc53e921e383906c26077403dd394d
Max Desiatov <max@desiatov.com>
2018-10-01 15:08:09 +0100

    Simplified index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,3 @@
-import { Link } from 'gatsby'
-
-import Layout from '../components/layout'
 import MDXRenderer from 'gatsby-mdx/mdx-renderer';
 
 export const pageQuery = graphql`
@@ -19,16 +16,6 @@ export const pageQuery = graphql`
 `
 
 export default function({ __mdxScope, data: {allMdx: {edges}} }) {
-  return (
-    <Layout>
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <Link to="/page-2/">Go to page 2</Link>
-      {edges.map(({node: {id, code}}) => 
-        <MDXRenderer
-          key={id}
-          scope={__mdxScope}>{code.body}</MDXRenderer>) }
-    </Layout>
-  );
+  return edges.map(({node: {id, code}}) => 
+        <MDXRenderer key={id} scope={__mdxScope}>{code.body}</MDXRenderer>);
 }
